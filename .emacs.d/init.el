693ad70a20333590a1da6cac12aea4a27590bd0a
Marc Joliet <marcec@gmx.de>
2013-08-04 19:28:19 +0200

    Emacs: set up spell checking with hunspell

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,6 +26,13 @@
 ; set up go mode
 (require 'go-mode-load)
 
+; set up spell checking, use hunspell instead of ispell
+(setq ispell-program-name "hunspell") 
+(require 'rw-language-and-country-codes)
+(require 'rw-ispell)
+(require 'rw-hunspell)
+(setq ispell-dictionary "de_DE_myspell")
+
 ; activate octave-mode for m-Files
 (add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
 
@@ -34,4 +41,8 @@
  ; in addition to the clipboard (used by default), also use the primary selection
  '(x-select-enable-primary t)
  ; automatically save the bookmarks file when creating bookmarks
- '(bookmark-save-flag 1))
+ '(bookmark-save-flag 1)
+ '(rw-hunspell-default-dictionary "de_DE_myspell")
+ '(rw-hunspell-dicpath-list (quote ("/usr/share/myspell")))
+ '(rw-hunspell-make-dictionary-menu t)
+ '(rw-hunspell-use-rw-ispell t))
