6c504825695f1d953f1cf9b6bb1763f9ae5f27fe
Marc Joliet <marcec@gmx.de>
2013-08-05 02:27:40 +0200

    Emacs: slight reordering of settings.
    This comes from editing the configuration using the custom variables UI from
    within Emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,23 +28,29 @@
 ; set up go mode
 (require 'go-mode-load)
 
+; activate octave-mode for m-Files
+(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
+
 ; set up spell checking, use hunspell instead of ispell
-(setq ispell-program-name "hunspell") 
+(setq ispell-program-name "hunspell")
 (require 'rw-language-and-country-codes)
 (require 'rw-ispell)
 (require 'rw-hunspell)
 (setq ispell-dictionary "de_DE_myspell")
 
-; activate octave-mode for m-Files
-(add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
-
 ; custom variables
 (custom-set-variables
- ; in addition to the clipboard (used by default), also use the primary selection
- '(x-select-enable-primary t)
  ; automatically save the bookmarks file when creating bookmarks
  '(bookmark-save-flag 1)
  '(rw-hunspell-default-dictionary "de_DE_myspell")
  '(rw-hunspell-dicpath-list (quote ("/usr/share/myspell")))
  '(rw-hunspell-make-dictionary-menu t)
- '(rw-hunspell-use-rw-ispell t))
+ '(rw-hunspell-use-rw-ispell t)
+ ; in addition to the clipboard (used by default), also use the primary selection
+ '(x-select-enable-primary t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
