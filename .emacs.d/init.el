2273017666fbb5085ad718ff530c7ead0ad741c0
Marc Joliet <marcec@gmx.de>
2013-08-23 14:55:23 +0200

    Emacs: add the MELPA package archive.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,6 +18,9 @@
 (add-to-list 'package-archives 
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives 
+    '("melpa" .
+      "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 ; must be after (package-initialize)
