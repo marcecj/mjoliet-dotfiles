8bc86b082b6d84428a34b041e0a7cea04a5c3e40
Marc Joliet <marcec@gmx.de>
2013-08-26 21:21:08 +0200

    FISH: migrate some bash/zsh settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,25 @@
 # unset the default greeting
 set fish_greeting
+
+# TODO: it seems fish is not started as a login shell. Why?
+# if status --is-login
+set -x MATLAB_ARCH glnx86
+
+set PATH $PATH "$HOME/bin/"
+
+## activate colorgcc - colorized gcc output
+#if [[ -z $(echo $PATH | grep colorgcc) && -d "/usr/lib/colorgcc/bin/" ]]; then
+#    set -x PATH "/usr/lib/colorgcc/bin/" $PATH
+#fi
+
+## mpc tries localhost, which is ipv6
+#set -x MPD_HOST ::1
+
+## File ledger uses
+#set -x LEDGER_FILE $HOME/finances.dat
+
+# setup a directory for finished python modules
+set -x PYTHONPATH $HOME/python
+
+# activate S3TC
+set -x R600_ENABLE_S3TC 1
