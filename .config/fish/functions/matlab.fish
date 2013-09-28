721233bb6025bbdd366d05359638ed8ae70a3d42
Marc Joliet <marcec@gmx.de>
2013-09-28 15:08:13 +0200

    Generalise matlab.fish to mixed 32/64 bit systems

--- .config/fish/functions/matlab.fish
+++ .config/fish/functions/matlab.fish
@@ -1,4 +1,10 @@
 function matlab -d "Starts MATLAB sanely."
-    set --export LD_PRELOAD /usr/lib/libportaudio.so /usr/lib/gcc/x86_64-pc-linux-gnu/(gcc -dumpversion)/libstdc++.so
-    command padsp matlab $argv
+    set LD_PRELOAD /usr/\$LIB/libportaudio.so /usr/\$LIB/pulseaudio/libpulsedsp.so
+    if test "$MATLAB_ARCH" -eq "glnx86" -a "(uname --machine)" -eq "x86_64"
+        set LD_PRELOAD $LD_PRELOAD /usr/lib/gcc/x86_64-pc-linux-gnu/(gcc -dumpversion)/32/libstdc++.so
+    else
+        set LD_PRELOAD $LD_PRELOAD /usr/lib/gcc/x86_64-pc-linux-gnu/(gcc -dumpversion)/libstdc++.so
+    end
+    set --export LD_PRELOAD
+    command matlab $argv
 end
