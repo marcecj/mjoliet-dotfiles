c0e73ac0da509729fab1db2af468113cb96071fe
Marc Joliet <marcec@gmx.de>
2013-09-28 15:37:04 +0200

    matlab.fish: omit the full path where possible
    I didn't realise one can leave out the full path to a library in LD_PRELOAD.
    This simplifies matlab.fish greatly.

--- .config/fish/functions/matlab.fish
+++ .config/fish/functions/matlab.fish
@@ -1,10 +1,4 @@
 function matlab -d "Starts MATLAB sanely."
-    set LD_PRELOAD /usr/\$LIB/libportaudio.so /usr/\$LIB/pulseaudio/libpulsedsp.so
-    if test "$MATLAB_ARCH" -eq "glnx86" -a "(uname --machine)" -eq "x86_64"
-        set LD_PRELOAD $LD_PRELOAD /usr/lib/gcc/x86_64-pc-linux-gnu/(gcc -dumpversion)/32/libstdc++.so
-    else
-        set LD_PRELOAD $LD_PRELOAD /usr/lib/gcc/x86_64-pc-linux-gnu/(gcc -dumpversion)/libstdc++.so
-    end
-    set --export LD_PRELOAD
+    set --export LD_PRELOAD libportaudio.so /usr/\$LIB/pulseaudio/libpulsedsp.so libstdc++.so
     command matlab $argv
 end
