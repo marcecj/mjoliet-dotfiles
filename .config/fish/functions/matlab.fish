58feef9323212d8753a0ae2ff7fdc5e6d68d833b
Marc Joliet <marcec@gmx.de>
2013-10-17 18:03:56 +0200

    Add a comment

--- .config/fish/functions/matlab.fish
+++ .config/fish/functions/matlab.fish
@@ -1,4 +1,7 @@
 function matlab -d "Starts MATLAB sanely."
+    # override some of MATLAB's bundled libraries with those from the system in
+    # order to prevent annoying run-time problems (such as portaudio not
+    # supporting JACK)
     set --export LD_PRELOAD libportaudio.so /usr/\$LIB/pulseaudio/libpulsedsp.so libstdc++.so
     command matlab $argv
 end
