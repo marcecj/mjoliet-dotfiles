f659d787990ecfb9d248082fcf60e4de4f112f43
Marc Joliet <marcec@gmx.de>
2014-03-06 12:24:33 +0100

    Emacs: decrease the default font size on Arthur

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -82,7 +82,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 83 :width normal)))))
 
 ; set the *global* dictionary to de_DE (NOTE: using this function ensures that
 ; the personal dictionary is correctly set according to
