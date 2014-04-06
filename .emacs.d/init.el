ac96ec97f6993d9f89c4d042a8f1da4012a3785b
Marc Joliet <marcec@gmx.de>
2014-04-06 21:37:35 +0200

    Emacs: delete extra whitespace

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,10 +16,10 @@
 
 ; add various package repositories
 (require 'package)
-(add-to-list 'package-archives 
+(add-to-list 'package-archives
     '("marmalade" .
       "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives 
+(add-to-list 'package-archives
     '("melpa" .
       "http://melpa.milkbox.net/packages/"))
 (package-initialize)
