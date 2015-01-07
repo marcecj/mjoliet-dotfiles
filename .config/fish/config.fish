e8c0d05615537757a6fda3f1dbe9dbb083358b21
Marc Joliet <marcec@gmx.de>
2015-01-07 23:48:28 +0100

    Change $MPD_HOST to marcec.fritz.box
    Arthur used to connect to marcec directly, but now it connects indirectly over
    our LAN.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ set fish_greeting
 set PATH $PATH "$HOME/bin/" "$HOME/.local/bin/"
 
 # set the default MPD host to my desktop machine
-set -x MPD_HOST marcec.marcnet
+set -x MPD_HOST marcec.fritz.box
 
 # override $JAVA_HOME if java-config exists (i.e., a Java runtime is installed);
 # this is for MATLAB, so that the GUI can start without crashing
