c9dcf73383073d70c590057ea9a7459b33b074d8
Marc Joliet <marcec@gmx.de>
2015-06-02 21:15:01 +0200

    Rename pa_tunnel_to_{marcec => thetick}

--- .config/fish/functions/pa_tunnel_to_thetick.fish
+++ .config/fish/functions/pa_tunnel_to_thetick.fish
@@ -1,10 +1,10 @@
-function pa_tunnel_to_marcec --description "Create a PA tunnel to marcec"
+function pa_tunnel_to_thetick --description "Create a PA tunnel to thetick"
 
     # Pass the short host name to make this function independent of the
     # particular network used, i.e., independent of the domain name.
     pacmd load-module module-tunnel-sink-new.so \
-        sink_name=marcec \
-        server=marcec \
+        sink_name=thetick \
+        server=thetick \
         sink=alsa_output.pci-0000_01_07.0.analog-stereo
 
 end
